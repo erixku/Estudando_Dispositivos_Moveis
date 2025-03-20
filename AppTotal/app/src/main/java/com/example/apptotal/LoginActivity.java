969d35c000ab979351cbfabb960f820c8ab21811
Alanasou <alanasou16@gmail.com>
2025-03-19 21:28:56 -0300

    ultima atualização 19/03/2025

--- AppTotal/app/src/main/java/com/example/apptotal/LoginActivity.java
+++ AppTotal/app/src/main/java/com/example/apptotal/LoginActivity.java
@@ -1,6 +1,12 @@
 package com.example.apptotal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +15,42 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button btLogin;
+    EditText edUser, edSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        btLogin   = (Button) findViewById(R.id.btLogin);
+
+        edUser = (EditText) findViewById(R.id.edUser);
+        edSenha = (EditText) findViewById(R.id.edSenha);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int senha;
+                String login;
+
+                senha  = Integer.parseInt(edSenha.getText().toString());
+                login = edUser.getText().toString();
+
+
+                if (senha == 123 && login.equals("Usuario")) {
+                    Intent telacadastro;
+                    telacadastro = new Intent(LoginActivity.this,MenuActivity.class);
+                    startActivity(telacadastro);
+                    finish();
+                }
+                else {
+
+                }
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- AppTotal/app/src/main/java/com/example/apptotal/MenuActivity.java
+++ AppTotal/app/src/main/java/com/example/apptotal/MenuActivity.java
@@ -1,6 +1,10 @@
 package com.example.apptotal;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +13,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MenuActivity extends AppCompatActivity {
+    Button btIMC;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_menu);
+
+        btIMC = findViewById(R.id.btIMC);
+
+        btIMC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent IMC;
+                IMC = new Intent(MenuActivity.this, IMCActivity.class);
+                startActivity(IMC);
+               // finish();
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
