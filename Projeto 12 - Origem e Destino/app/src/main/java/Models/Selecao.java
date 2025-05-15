ca300a951b20f3f23604fe0475251a95ea100ba4
erixku <erickgomes1010@outlook.com>
2025-05-14 22:02:40 -0300

    Checkbox atualizado e pesquisas estruturadas

---
+++ Projeto 12 - Origem e Destino/app/src/main/java/Models/Selecao.java
@@ -0,0 +1,8 @@
+package Models;
+
+public class Selecao {
+        public static int totalSelecionados = 0;
+        public static String origem = null;
+        public static String destino = null;
+    }
+}
--- Projeto 12 - Origem e Destino/app/src/main/java/com/example/origemedestino/MainActivity.java
+++ Projeto 12 - Origem e Destino/app/src/main/java/com/example/origemedestino/MainActivity.java
@@ -74,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
         // Inicia a animação na ImageView do trem
         trainImageView.startAnimation(animation);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.rbTucuruvi), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
